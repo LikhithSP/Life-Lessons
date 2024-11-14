d5331b08d0ac1cbe8342ede7c11373c089a59677
Likhith SP <splikith04@gmail.com>
2024-11-14 22:22:58 +0530

    added simple layout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import axios from "axios";
 import './App.css';
 
 class App extends React.Component{
-  state = { lesson: ''};
+  state = { advice: '' };
+
   componentDidMount(){
-    
+    this.fetchLesson();
   }
   
- fetchLesson = () => {
-  axios.get('https://api.adviceslip.com/advice');
- }
+  fetchLesson = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      }) 
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   render(){
     return(
-    <h1>APP</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+        </div>
+        <button onClick={this.fetchLesson}>Give Me Another Advice</button>
+      </div>
     );
   }
 }
